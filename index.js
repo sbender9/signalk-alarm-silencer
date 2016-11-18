ddde0f1030ab6ad7845c3cccc90b18ae33be70d7
Scott Bender <scott@scottbender.net>
2016-11-17 20:16:49 -0500

    Initial checkin

---
+++ index.js
@@ -0,0 +1,80 @@
+const debug = require('debug')('alarm-silencer')
+
+const _ = require('lodash')
+
+module.exports = function(app) {
+  var plugin = {}
+  var unsubscribes = []
+
+  plugin.id = "alarmsilencer"
+  plugin.name = "Alarm Silencer"
+  plugin.description = "Plugin to silence SignalK Alarms"
+
+  plugin.schema = {
+    title: "Alarm Silencer",
+    type: "object",
+    required: [
+    ],
+    properties: {
+    }
+  }
+
+
+  plugin.start = function(options)
+  {
+    return true
+  }
+
+  plugin.registerWithRouter = function(router) {
+    router.post("/silenceNotification", (req, res) => {
+
+      notification = req.body
+      if ( typeof notification.path == 'undefined' )
+      {
+        debug("invalid request: " + util.inspect(notification, {showHidden: false, depth: 1}))
+        res.status(400)
+        res.send("Invalid Request")
+        return
+      }
+
+      var existing = _.get(app.signalk.self, notification.path)
+      debug("existing: " + existing.method)
+      if ( typeof existing.method == "undefined"
+           || existing.method == null
+           || existing.method.indexOf("sound") != -1 )
+      {
+        if  ( typeof existing.method != "undefined" && existing.method != null )
+          existing.method = existing.filter(function(method) { return method != "sound" })
+        else
+          existing.method = ['visual']
+        
+        existing.timestamp = (new Date()).toISOString()
+
+        const delta = {
+          context: "vessels." + app.selfId,
+          updates: [
+            {
+              source: {
+                label: "self.notificationhandler"
+              },
+              values: [{
+                path: notification.path,
+                value: existing
+              }]
+            }
+          ]
+        }
+        app.signalk.addDelta(delta)
+
+        debug("silenced alarm: " + notification.path)
+      }
+      res.send("Alarm silenced")
+    })
+  }
+  
+
+  plugin.stop = function() {
+  }
+
+  return plugin
+}
