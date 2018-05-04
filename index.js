726402be87f83f66180130a700393fc94f3ab2ee
Scott Bender <scott@scottbender.net>
2018-05-04 17:12:21 -0400

    chore: update to latest server apis

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const debug = require('debug')('alarm-silencer')
-const util = require('util')
+/**
+ * Copyright 2018 Scott Bender (scott@scottbender.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 const _ = require('lodash')
 
@@ -23,64 +36,67 @@ module.exports = function(app) {
 
   plugin.start = function(options)
   {
+    /*
+    var command = {
+      context: "vessels.self",
+      subscribe: [{
+        path: "requested.notifications.*",
+        policy: 'instant'
+      }]
+    }
+    
+    app.subscriptionmanager.subscribe(command, unsubscribes, subscription_error, got_delta)
+    */
+    
     return true
   }
 
   plugin.registerWithRouter = function(router) {
     router.post("/silenceNotification", (req, res) => {
 
-      notification = req.body
+      var notification = req.body
       if ( typeof notification.path == 'undefined' )
       {
-        debug("invalid request: " + util.inspect(notification, {showHidden: false, depth: 1}))
+        app.debug("invalid request: %j", notification)
         res.status(400)
         res.send("Invalid Request")
         return
       }
 
-      var existing = _.get(app.signalk.self, notification.path)
-      debug("existing: " + existing.method)
-      //if ( typeof existing.method == "undefined"
-        //|| existing.method == null )
-      {
-        /*
-        if  ( typeof existing.method != "undefined" && existing.method != null )
-        {
-          var idx = existing.method.indexOf("sound")
-          if ( idx != -1 )
-            existing.method.splice(idx, 1)
-        }
-        else
-          existing.method = ['visual']
-        */
-        existing.method = []
-        
-        existing.timestamp = (new Date()).toISOString()
-
-        const delta = {
-          context: "vessels." + app.selfId,
-          updates: [
-            {
-              source: {
-                label: "self.notificationhandler"
-              },
-              values: [{
-                path: notification.path,
-                value: existing
-              }]
-            }
-          ]
-        }
-        app.handleMessage(plugin.id, delta)
-
-        debug("silenced alarm: " + JSON.stringify(delta))
-      }
+      silenceAlarm(notification.path)
+      
       res.send("Alarm silenced")
     })
   }
   
 
   plugin.stop = function() {
+  }
+
+  function silenceAlarm(path) {
+    var existing = app.getSelfPath(path)
+    app.debug("existing: " + existing.method)
+    existing.method = []
+        
+    existing.timestamp = (new Date()).toISOString()
+    
+    const delta = {
+      context: "vessels." + app.selfId,
+      updates: [
+        {
+          source: {
+            label: "self.notificationhandler"
+          },
+          values: [{
+            path: notification.path,
+            value: existing
+          }]
+        }
+      ]
+    }
+    app.handleMessage(plugin.id, delta)
+    
+    app.debug("silenced alarm: %j", delta)
   }
 
   return plugin
